1c3680b460556f45db41398e1139d81886368862
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2021-01-10 15:57:15 +0300

    Add files

---
+++ config.py
@@ -0,0 +1,25 @@
+import tweepy
+import logging
+import os
+from os import environ
+logger = logging.getLogger()
+#auth function
+
+def create_api():
+	consumer_key = environ["CONSUMER_KEY"]
+	consumer_secret = environ["CONSUMER_SECRET"]
+	access_token = environ["ACCESS_TOKEN"]
+	access_token_secret = environ["ACCESS_TOKEN_SECRET"]
+
+	auth = tweepy.OAuthHandler(consumer_key , consumer_secret)
+	auth.set_access_token(access_token , access_token_secret)
+	api = tweepy.API(auth , wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+	try:
+		api.verify_credentials()
+	except Exception as e:
+		logger.error("Error creating API" , exc_info=True)
+		raise e
+	logger.info('API Created')
+	return api
+
+
